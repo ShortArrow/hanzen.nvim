ca800d8709dc19197597c2f7d41ea955e8dc5c9a
ShortArrow <bamboogeneral@shortarrow.jp>
2024-04-27 19:30:21 +0900

    update reverse function

--- lua/hanzen/init.lua
+++ lua/hanzen/init.lua
@@ -10,16 +10,16 @@ function M.reverse_text()
   local _, end_row, end_col, _ = unpack(vim.fn.getpos("'>"))
 
   -- Get text at range
-  local lines = vim.api.nvim_buf_get_text(0, start_row - 1, start_col, end_row - 1, end_col, {})
+  local target_lines = vim.api.nvim_buf_get_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, {})
 
   -- Concat text
-  local text = table.concat(lines, "\n")
+  local concat_text = table.concat(target_lines, "\n")
 
   -- process text here
-  local processed_text = text:reverse()
+  local processed_text = concat_text:reverse()
 
   -- Rewrite text
-  vim.api.nvim_buf_set_text(0, start_row - 1, start_col, end_row - 1, end_col, vim.split(processed_text, "\n"))
+  vim.api.nvim_buf_set_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, vim.split(processed_text, "\n"))
 end
 
 function M.setup()
@@ -28,7 +28,7 @@ function M.setup()
     require 'hanzen'.reverse_text()
   end, { range = true })
   vim.api.nvim_create_user_command('HanzenInfo', function()
-    require 'hanzen'.process_text()
+    require 'hanzen'.hello()
   end, { range = true })
 end
 
