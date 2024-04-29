7eb0df232c87f2bab0f6da759833c861dc64c9a7
ShortArrow <bamboogeneral@shortarrow.jp>
2024-04-29 16:12:54 +0900

    add hankaku len test

--- tests/katakana_spec.lua
+++ tests/katakana_spec.lua
@@ -7,12 +7,14 @@ describe("katakana",
       assert.equals(2, vim.api.nvim_strwidth(target))
       assert.equals(2, vim.fn.strdisplaywidth(target))
     end)
+    it("hankaku len", function()
+      local target = "ｱ"
+      assert.equals(3, string.len(target))
+      assert.equals(1, vim.api.nvim_strwidth(target))
+      assert.equals(1, vim.fn.strdisplaywidth(target))
+    end)
     it("parse_katakana", function()
       local target = "アイウエオ"
-      assert.equals("ｱｲｳｴｵ", katakana.parse_katakana(target))
-    end)
-    it("kakikukeko", function()
-      local target = "カキクケコ"
       assert.equals("ｱｲｳｴｵ", katakana.parse_katakana(target))
     end)
   end
