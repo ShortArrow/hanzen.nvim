5e073b8a6125b32c0286e76768e596d6a89c9bfd
ShortArrow <bamboogeneral@shortarrow.jp>
2024-04-30 12:25:47 +0900

    add katakana byte check test

--- tests/katakana_spec.lua
+++ tests/katakana_spec.lua
@@ -17,5 +17,17 @@ describe("katakana",
       local target = "アイウエオ"
       assert.equals("ｱｲｳｴｵ", katakana.parse_katakana(target))
     end)
+    it("firstbyte", function()
+      local target = "アイウエオ"
+      local firstbyte = target:sub(1, 1)
+      local byte = string.byte(firstbyte)
+      assert.equals(0xE3, byte)
+      local secondbyte = target:sub(2, 2)
+      byte = string.byte(secondbyte)
+      assert.equals(0x82, byte)
+      local thirdbyte = target:sub(3, 3)
+      byte = string.byte(thirdbyte)
+      assert.equals(0xA2, string.byte(thirdbyte))
+    end)
   end
 )
