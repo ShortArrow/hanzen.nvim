58264f79cbf7f2340f221053f67fab4ec57422ae
ShortArrow <bamboogeneral@shortarrow.jp>
2024-05-03 10:48:25 +0900

    update command half_to_full

--- lua/hanzen/command.lua
+++ lua/hanzen/command.lua
@@ -2,7 +2,6 @@ local M = {
   katakana = {}
 }
 
--- アイウエオカキクケコ
 local feature = require 'hanzen.feature'
 local katakana = require 'hanzen.katakana'
 
@@ -40,15 +39,16 @@ end
 ---Rewrite Hankaku katakana to Zenkaku katakana
 function M.katakana.half_to_full()
   local start_row, start_col, end_row, end_col = feature.get_selected_range()
-  local concat_text = feature.get_selected_text(start_row, start_col, end_row, end_col)
-  print(concat_text)
+  local end_byte = feature.get_byte_at_position(end_row, end_col)
+  local detectd_byte_length = feature.get_byte_length_from_firstone(end_byte)
+  local concat_text = feature.get_selected_text(start_row, start_col, end_row, end_col + detectd_byte_length - 1)
 
   -- process text here
   local processed_text = katakana.half_to_full(concat_text)
-  print(processed_text)
 
   -- Rewrite text
-  vim.api.nvim_buf_set_text(0, start_row, start_col, end_row, end_col, vim.split(processed_text, "\n"))
+  vim.api.nvim_buf_set_text(0, start_row, start_col, end_row, end_col + detectd_byte_length - 1,
+    vim.split(processed_text, "\n"))
 end
 
 return M
