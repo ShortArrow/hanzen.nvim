81d2119d9c663c59a43657896f0e549276290b00
ShortArrow <bamboogeneral@shortarrow.jp>
2024-05-03 10:30:55 +0900

    update command full_to_half

--- lua/hanzen/command.lua
+++ lua/hanzen/command.lua
@@ -2,6 +2,7 @@ local M = {
   katakana = {}
 }
 
+-- アイウエオカキクケコ
 local feature = require 'hanzen.feature'
 local katakana = require 'hanzen.katakana'
 
@@ -24,22 +25,27 @@ end
 ---Rewrite Zenkaku katakana to Hankaku katakana
 function M.katakana.full_to_half()
   local start_row, start_col, end_row, end_col = feature.get_selected_range()
-  local concat_text = feature.get_selected_text(start_row, start_col, end_row, end_col)
+  local end_byte = feature.get_byte_at_position(end_row, end_col)
+  local detectd_byte_length = feature.get_byte_length_from_firstone(end_byte)
+  local concat_text = feature.get_selected_text(start_row, start_col, end_row, end_col + detectd_byte_length - 1)
 
   -- process text here
   local processed_text = katakana.full_to_half(concat_text)
 
   -- Rewrite text
-  vim.api.nvim_buf_set_text(0, start_row, start_col, end_row, end_col, vim.split(processed_text, "\n"))
+  vim.api.nvim_buf_set_text(0, start_row, start_col, end_row, end_col + detectd_byte_length - 1,
+    vim.split(processed_text, "\n"))
 end
 
 ---Rewrite Hankaku katakana to Zenkaku katakana
 function M.katakana.half_to_full()
   local start_row, start_col, end_row, end_col = feature.get_selected_range()
   local concat_text = feature.get_selected_text(start_row, start_col, end_row, end_col)
+  print(concat_text)
 
   -- process text here
   local processed_text = katakana.half_to_full(concat_text)
+  print(processed_text)
 
   -- Rewrite text
   vim.api.nvim_buf_set_text(0, start_row, start_col, end_row, end_col, vim.split(processed_text, "\n"))
--- lua/hanzen/feature.lua
+++ lua/hanzen/feature.lua
@@ -14,6 +14,30 @@ function M.get_selected_range()
   return start_row, start_col, end_row, end_col
 end
 
+function M.get_byte_at_position(row, col)
+  -- Get the line at the specified row
+  local line = vim.api.nvim_buf_get_lines(0, row, row + 1, false)[1]
+  -- Get the byte at the specified column
+  local byte = string.byte(line, col)
+  return byte
+end
+
+function M.get_byte_length_from_firstone(end_byte)
+  if end_byte > 0x7F then
+    if end_byte >= 0xC0 and end_byte <= 0xDF then     -- 0b110xxxxx
+      return 2
+    elseif end_byte >= 0xE0 and end_byte <= 0xEF then -- 0b1110xxxx
+      return 3
+    elseif end_byte >= 0xF0 and end_byte <= 0xF7 then -- 0b11110xxx
+      return 4
+    else
+      error("Invalid Byte: " .. end_byte)
+    end
+  else
+    return 1
+  end
+end
+
 ---Get user selected text
 ---@param start_row number selected text start row
 ---@param start_col number selected text start col
